0d4e274f36fb5477a047b812c4c8034b66e4e2ee
Tarbiya131 <79188735+Tarbiya131@users.noreply.github.com>
2022-09-21 23:03:40 +0600

    HWJS12

---
+++ script.js
@@ -0,0 +1,59 @@
+class User {
+    constructor(name, surname){
+        this.name = name;
+        this.surname = surname;
+    }
+    getFullName(){
+        console.log(`${this.name} ${this.surname}`)
+    }
+}
+let date = new Date();
+date = date.getFullYear();
+
+class Student extends User {
+    constructor(name, surname, year){
+        super(name, surname);
+        this.year = year;
+    }
+    getCourse(){
+        if(this.year > (date - 5)){
+        console.log((date + 1) - this.year)
+        }else console.log(`Graduated from college in ${this.year + 5}`);
+    }
+}
+
+let user = new User("Tarbiya", "Akhmetzhanova");
+let student = new Student("Tarbiya", "Akhmetzhanova", 1994);
+let student2 = new Student("Aliya", "Akhmetzhanova", 1993);
+let student3 = new Student("Ramiz", "Schajchin", 2014);
+
+student.getFullName();
+student.getCourse();
+student2.getFullName();
+student2.getCourse();
+student3.getFullName();
+student3.getCourse();
+
+class Rectangle {
+    constructor(width, height){
+        this.width = width + "px";
+        this.height = height + "px";
+    };
+    setWidth(width){
+        this.width = width  + "px";
+    };
+    getWidth(){
+        return this.width;
+    };
+    setHeight(height){
+        this.height = height + "px";
+    };
+    getHeight(){
+        return this.height;
+    };
+}
+ let rectangle = new Rectangle(200, 350);
+ console.log(rectangle);
+ rectangle.setWidth(500)
+ rectangle.setHeight(700);
+ console.log(rectangle);
